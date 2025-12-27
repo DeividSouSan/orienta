0f53b8d39517d394fd69d36a99853eb67b443331
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-12-27 13:43:05 -0300

    test: simplify test

--- tests/integration/sessions/test_sessions_delete.py
+++ tests/integration/sessions/test_sessions_delete.py
@@ -1,33 +1,22 @@
 from datetime import datetime, timezone
 from http.cookies import SimpleCookie
-from dotenv import load_dotenv
-import os
 import requests
-
-load_dotenv()
-API_URL = os.getenv("API_URL", "http://localhost:5000/api/v1")
 
 
 def test_delete_session():
-    s = requests.Session()
+    response = requests.delete("http://localhost:5000/api/v1/sessions")
 
-    response1 = s.post(
-        API_URL + "/sessions", json={"email": "mock@orienta.com", "password": "123456"}
-    )
-
-    assert response1.status_code == 201
-    assert response1.headers.get("Set-Cookie") is not None
-
-    response2 = s.delete(API_URL + "/sessions")
+    assert response.status_code == 200
 
     cookie = SimpleCookie()
-    cookie.load(response2.headers.get("Set-Cookie"))
+    cookie.load(response.headers.get("Set-Cookie"))
 
-    sid = cookie["session_id"]
-    assert sid.value == ""
-    assert sid["max-age"] == "0"
+    session_id = cookie["session_id"]
+    assert session_id.value == ""
+    assert session_id["max-age"] == "0"
 
-    exp = datetime.strptime(sid["expires"], "%a, %d %b %Y %H:%M:%S GMT").replace(
-        tzinfo=timezone.utc
-    )  # adiciona um offset ao expires que era naive
-    assert exp < datetime.now(timezone.utc)
+    expires = datetime.strptime(
+        session_id["expires"], "%a, %d %b %Y %H:%M:%S GMT"
+    ).replace(tzinfo=timezone.utc)  # adiciona um offset ao expires que era naive
+
+    assert expires < datetime.now(timezone.utc)
