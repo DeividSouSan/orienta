41cdc04a9dbb2fffa895a70c9442d6f2f652616e
Deivid Souza Santana <deividsantana2013@gmail.com>
2026-01-11 13:52:29 -0300

    test: add missing GET test with unauthorized user to `/api/v1/user`

--- tests/integration/user/test_GET_user.py
+++ tests/integration/user/test_GET_user.py
@@ -1,10 +1,8 @@
-def test_get_current_user(auth_client, new_user):
+def test_with_authenticated_user(auth_client, new_user):
     response = auth_client.get("/api/v1/user")
-
-    assert response.status_code == 200
-
     response_body = response.get_json()
 
+    assert response.status_code == 200
     assert response_body == {
         "message": "Usuário atual recuperado com sucesso.",
         "data": {
@@ -13,4 +11,17 @@ def test_get_current_user(auth_client, new_user):
             "email": new_user["email"],
             "created_at": response_body["data"]["created_at"],
         },
+    }
+
+
+def test_with_unauthorized_user(client):
+    response = client.get("/api/v1/user")
+    response_body = response.get_json()
+
+    assert response.status_code == 401
+    assert response_body == {
+        "name": "UnauthorizedError",
+        "message": "Cookie de sessão não encontrado.",
+        "action": "Faça login para continuar.",
+        "code": 401,
     }
